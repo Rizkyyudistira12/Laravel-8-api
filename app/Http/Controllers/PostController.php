871d54575474ecf5fc0d028e06acbff83df6883a
Muchammad Rizky Yudistira <mrizkyyudistira@gmail.com>
2020-11-10 10:34:26 +0700

    First Commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return response()->json([
+            'success' => true,
+            'message' => 'List Data Post',
+            'data' => $posts
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrfail($id);
+        return response()->json([
+            'success' => true,
+            'message' => 'List Data Post',
+            'data' => $post
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        if($post)
+        {
+            return response()->json([
+                'success' => true,
+                'message' => 'Post created',
+                'data' => $post
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Post Failed to Save',
+        ], 409);
+    }
+
+
+    public function update(Request $request, Post $post)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $post = Post::findOrFail($post->id);
+
+        if($post)
+        {
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Post Updated',
+                'data'    => $post  
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Post Not Found',
+        ], 404);
+
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrfail($id);
+
+        if($post)
+        {
+            $post->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Post Deleted',
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Post Not Found',
+        ], 404);
+    }
+
+
+}
